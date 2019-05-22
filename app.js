448b7d83b2df543a674c4e0fc87a116a04ea5573
Subham Goyal <subhamiitk@gmail.com>
2019-05-22 12:47:55 +0530

    Created Restful api

---
+++ app.js
@@ -0,0 +1,87 @@
+const Joi = require("@hapi/joi");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+const genres = [
+  { id: 1, name: "Horror" },
+  { id: 2, name: "Sci-Fi" },
+  { id: 3, name: "Comedy" }
+];
+
+// HTTP Get request for all genres
+app.get("/api/genres", (req, res) => {
+  res.send(genres);
+});
+
+// Get request for a genre's ID
+app.get("/api/genres/:id", (req, res) => {
+  const genre = genres.find(c => c.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found");
+  res.send(genre);
+});
+
+// POST request
+app.post("/api/genres", (req, res) => {
+  const schema = {
+    name: Joi.string()
+      .min(4)
+      .required()
+  };
+
+  const result = Joi.validate(req.body, schema);
+  if (result.error)
+    return res.status(400).send(result.error.details[0].message);
+
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name
+  };
+
+  genres.push(genre);
+  res.send(genre);
+});
+
+// PUT request
+app.put("/api/genres/:id", (req, res) => {
+  const genre = genres.find(c => c.id === parseInt(req.params.id));
+
+  if (!genre)
+    return res
+      .status(404)
+      .send("The genre request with the given ID was not found");
+
+  const schema = {
+    name: Joi.string()
+      .min(4)
+      .required()
+  };
+
+  const result = Joi.validate(req.body, schema);
+
+  if (result.error)
+    return res.status(400).send(result.error.details[0].message);
+
+  genre.name = req.body.name;
+  res.send(genre);
+});
+
+// DELETE
+app.delete("/api/genres/:id", (req, res) => {
+  const genre = genres.find(c => c.id === parseInt(req.params.id));
+  if (!genre)
+    return res
+      .status(404)
+      .send("The genre requested for the given ID was not found");
+
+  const index = genres.indexOf(genre);
+  genres.slice(index, 1);
+  res.send(genre);
+});
+
+// PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening to port ${port}...`);
+});
