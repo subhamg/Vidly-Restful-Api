ab6cf81bba0012cf3ab389ff9fc7e2d11307cf0e
Subham Goyal <subhamiitk@gmail.com>
2019-05-28 12:38:55 +0530

    Generated authentication token using jsonwebtoken library

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+const Joi = require("@hapi/joi");
 const bcrypt = require("bcrypt");
 const _ = require("lodash");
 const { User } = require("../models/user");
@@ -6,7 +8,7 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  const { error } = validateUser(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
@@ -15,7 +17,9 @@ router.post("/", async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send("Invalid email or password");
 
-  res.send(true);
+  const token = jwt.sign({ _id: user._id }, "jwtPrivateKey");
+
+  res.send(token);
 });
 
 function validate(req) {
@@ -30,7 +34,7 @@ function validate(req) {
       .required()
   };
 
-  return Joi.validate(user, Schema);
+  return Joi.validate(req, Schema);
 }
 
 module.exports = router;
