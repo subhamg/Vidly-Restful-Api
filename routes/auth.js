b5f0aae505ab44531f47b0723487ce88a982b6e4
Subham Goyal <subhamiitk@gmail.com>
2019-05-28 12:18:06 +0530

    Authenticating users

---
+++ routes/auth.js
@@ -0,0 +1,36 @@
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
+const { User } = require("../models/user");
+const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const { error } = validateUser(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid email or password");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid email or password");
+
+  res.send(true);
+});
+
+function validate(req) {
+  const Schema = {
+    email: Joi.string()
+      .min(5)
+      .max(255)
+      .required(),
+    password: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+  };
+
+  return Joi.validate(user, Schema);
+}
+
+module.exports = router;
